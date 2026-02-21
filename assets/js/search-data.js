d8fb007b612bfa186ab999279520f33f72ac1776
fazlayofficial <fazlay.rabby.official@gmail.com>
2026-02-21 09:17:46 +0000

    Deploying to gh-pages from @ fazlayofficial/fazlayofficial.github.io@05dd5737cbaf0e9ee5a7f31641686f91c5085785 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,7 +518,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%66%61%7A%6C%61%79.%72%61%62%62%79.%6F%66%66%69%63%69%61%6C@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
