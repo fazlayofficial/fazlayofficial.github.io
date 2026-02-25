9bc99c2c75c70df498ffdd937f9a891fa638b85b
fazlayofficial <fazlay.rabby.official@gmail.com>
2026-02-25 19:56:37 +0000

    Deploying to gh-pages from @ fazlayofficial/fazlayofficial.github.io@7a285a8f4993d7f8c820f3766452b159503009b8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Curriculum Vitae of Fazlay Rabby – Embedded Machine Learning Researcher specializing in TinyML and Edge AI for robotics and healthcare.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
