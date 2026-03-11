2a0d39ef30164fce91008e602826782f029836a1
fazlayofficial <fazlay.rabby.official@gmail.com>
2026-03-11 14:28:20 +0000

    Deploying to gh-pages from @ fazlayofficial/fazlayofficial.github.io@f36506af385bbdca35af07b33ae9a140350e1d83 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -64,51 +64,21 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-wearable-motor-behavior-monitoring-for-asd",
+          title: 'Wearable Motor Behavior Monitoring for ASD',
+          description: "A continuous, passive IMU sensing platform using Fitbit Versa 3 for ASD behavioral monitoring.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/1_fitbit/";
+            },},{id: "projects-evo-extractor-framework",
+          title: 'Evo_Extractor Framework',
+          description: "A Framework for High-Fidelity sEMG Signal Acquisition.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/2_evo/";
+            },},{id: "projects-adaptive-tinyml-semg-wheelchair-control",
+          title: 'Adaptive TinyML sEMG Wheelchair Control',
+          description: "Designing a TinyML framework to control a robotic wheelchair using sEMG signals.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/3_wheelchair/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
